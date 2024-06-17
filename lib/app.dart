3990a939a9170d73cba7154d2af7662332f96183
nik007frontbit <nikhilhirpara04.frontbitstd@gmail.com>
2024-06-17 15:48:42 +0530

    flavour with notification

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'main_production.dart';
 
 class App extends StatelessWidget {
   final String flavour;
 
   const App({super.key, required this.flavour});
+
+  Future<void> showNotification() async {
+    AndroidNotificationDetails androidNotificationDetails =
+    const AndroidNotificationDetails(
+      "notification-leaning",
+      "Local Notification",
+      priority: Priority.max,
+      importance: Importance.high,
+    );
+    DarwinNotificationDetails darwinNotificationDetails =
+    const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+
+    await flutterLocalNotificationsPlugin.show(
+        0, "First notification", "Here i am boy", notificationDetails,
+        payload: "notification-0");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +37,11 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(flavour),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: showNotification,
+          tooltip: 'Notification',
+          child: const Icon(Icons.notification_add),
         ),
       ),
     );
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flavours_learn/app.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'api.dart';
 import 'firebase_options.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,5 +18,45 @@ Future<void> main() async {
 
   // await FirebaseApi().initNotification();
   FirebaseApi().initNoti();
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  //only local notification
+  AndroidInitializationSettings androidInitializationSettings =
+      const AndroidInitializationSettings("@mipmap/ic_launcher");
+  DarwinInitializationSettings darwinInitializationSettings =
+      const DarwinInitializationSettings(
+    requestBadgePermission: true,
+    requestAlertPermission: true,
+    requestCriticalPermission: true,
+    requestSoundPermission: true,
+  );
+  InitializationSettings initializationSettings = InitializationSettings(
+    android: androidInitializationSettings,
+    iOS: darwinInitializationSettings,
+  );
+  bool? initialized =
+      await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
   runApp(const App(flavour: "Production"));
+}
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message");
 }
